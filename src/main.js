658d874e16d55e0ba33da903cedfdacd9fa9f307
Henrique Lorenzi <hlorenzi12@gmail.com>
2018-08-23 00:05:17 -0300

    improve platforming

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let canvasHeight = 0
 
 let solver = null
 let player = null
-let polygonData = null
+let movingPlatforms = null
 
 
 let input =
@@ -34,20 +34,6 @@ function main()
 			new Vec2(Math.random() * canvasWidth, Math.random() * canvasHeight),
 			new Vec2(Math.random() * canvasWidth, Math.random() * canvasHeight)))
 	}*/
-	
-	polygonData = []
-	for (let i = 0; i < 10; i++)
-		polygonData.push(
-		{
-			x: Math.random() * canvasWidth,
-			y: Math.random() * canvasHeight,
-			w: Math.random() * 200,
-			h: Math.random() * 200,
-			edgeNum: 3 + Math.floor(Math.random() * 9),
-			rotation: Math.random() * Math.PI * 2,
-			rotationSpeed: (Math.random() * 2 - 1) * 0.05
-		})
-		
 	solver.polygons.push(new SolverPolygon([
 		new Vec2(0, 300),
 		new Vec2(300, 300),
@@ -60,24 +46,83 @@ function main()
 		new Vec2(300, 600)]))
 	solver.polygons.push(new SolverPolygon([
 		new Vec2(600, 200),
-		new Vec2(900, 300),
-		new Vec2(900, 600),
+		new Vec2(750, 300),
+		new Vec2(750, 600),
 		new Vec2(600, 600)]))
+	solver.polygons.push(new SolverPolygon([
+		new Vec2(750, 300),
+		new Vec2(900, 550),
+		new Vec2(900, 600),
+		new Vec2(750, 600)]))
+	solver.polygons.push(new SolverPolygon([
+		new Vec2(900, 550),
+		new Vec2(930, 550),
+		new Vec2(930, 600),
+		new Vec2(900, 600)]))
 	
 	solver.polygons.push(new SolverPolygon([
 		new Vec2(150, 200),
 		new Vec2(300, 200),
 		new Vec2(300, 210),
 		new Vec2(150, 210)]))
+	solver.polygons.push(new SolverPolygon([
+		new Vec2(450, 150),
+		new Vec2(600, 100),
+		new Vec2(600, 110),
+		new Vec2(450, 160)]))
 		
+	solver.polygons.push(new SolverPolygon([
+		new Vec2(850, 250),
+		new Vec2(900, 0),
+		new Vec2(910, 450),
+		new Vec2(900, 450)]))
 		
-	player = new Player(new Vec2(canvasWidth / 2, canvasHeight / 2))
+	movingPlatforms = []
+	movingPlatforms.push(
+	{
+		x: 1100,
+		y: 600,
+		w: 100,
+		h: 100,
+		edgeNum: 16,
+		rotation: 0,
+		rotationSpeed: 0.01,
+		polygons: [ new SolverPolygon([]), new SolverPolygon([]), new SolverPolygon([]), new SolverPolygon([]), new SolverPolygon([]) ]
+	})
+	
+	for (let m of movingPlatforms)
+		for (let p of m.polygons)
+			solver.polygons.push(p)
+	
+	/*for (let i = 0; i < 10; i++)
+		polygonData.push(
+		{
+			x: Math.random() * canvasWidth,
+			y: Math.random() * canvasHeight,
+			w: Math.random() * 200,
+			h: Math.random() * 200,
+			edgeNum: 3 + Math.floor(Math.random() * 9),
+			rotation: Math.random() * Math.PI * 2,
+			rotationSpeed: (Math.random() * 2 - 1) * 0.05
+		})*/
+		
+	player = new Player(new Vec2(canvasWidth / 2, 100))
 	
 	draw()
 	
 	window.onkeydown = (ev) => onKey(ev, true)
 	window.onkeyup = (ev) => onKey(ev, false)
+	window.onresize = (ev) => onResize()
 	window.requestAnimationFrame(step)
+	
+	onResize()
+}
+
+
+function onResize()
+{
+	//canvasWidth = canvas.width = window.innerWidth
+	//canvasHeight = canvas.height = window.innerHeight
 }
 
 
@@ -135,6 +180,27 @@ function step()
 			polygon.rotation))
 	}*/
 	
+	for (let movingPlatform of movingPlatforms)
+	{
+		movingPlatform.rotation += movingPlatform.rotationSpeed
+		
+		movingPlatform.polygons[0].vertices = SolverPolygon.regularPolygon(
+			movingPlatform.x, movingPlatform.y,
+			movingPlatform.w, movingPlatform.h,
+			movingPlatform.edgeNum, movingPlatform.rotation).vertices
+			
+		for (let i = 0; i < 4; i++)
+		{
+			let angle = i * Math.PI * 2 / 4 + movingPlatform.rotation
+			
+			movingPlatform.polygons[i + 1].vertices = SolverPolygon.regularPolygon(
+				movingPlatform.x + Math.cos(angle) * movingPlatform.w,
+				movingPlatform.y + Math.sin(angle) * movingPlatform.w,
+				movingPlatform.w * 0.5, movingPlatform.w * 0.5,
+				4, angle + Math.PI / 4).vertices
+		}
+	}
+	
 	player.step(input, solver)
 	
 	draw()
@@ -163,6 +229,9 @@ function draw()
 	
 	for (let polygon of solver.polygons)
 	{
+		if (polygon.vertices.length == 0)
+			continue
+		
 		ctx.beginPath()
 		ctx.moveTo(polygon.vertices[0].x, polygon.vertices[0].y)
 		
@@ -199,7 +268,7 @@ function draw()
 		}
 	}*/
 	
-	let raycast = solver.raycast(player.position.add(new Vec2(0, -player.radius - 10)), new Vec2(0, 1), player.radius - 0.1)
+	/*let raycast = solver.raycast(player.position.add(new Vec2(0, -player.radius - 10)), new Vec2(0, 1), player.radius - 0.1)
 	if (raycast != null)
 	{
 		ctx.strokeStyle = "#ccc"
@@ -217,7 +286,13 @@ function draw()
 		ctx.beginPath()
 		ctx.arc(raycast.point.x, raycast.point.y, 2, 0, Math.PI * 2)
 		ctx.stroke()
-	}
+		
+		ctx.strokeStyle = "#f80"
+		ctx.beginPath()
+		ctx.moveTo(raycast.point.x, raycast.point.y)
+		ctx.lineTo(raycast.point.x + raycast.normal.x * 15, raycast.point.y + raycast.normal.y * 15)
+		ctx.stroke()
+	}*/
 	
 	ctx.strokeStyle = "#00f"
 	ctx.beginPath()
--- src/player.js
+++ src/player.js
@@ -6,6 +6,7 @@ class Player
 		this.radius = 15
 		
 		this.onGround = false
+		this.onGroundLast = false
 		this.jumping = false
 		
 		this.speedGravity = new Vec2(0, 0)
@@ -17,6 +18,9 @@ class Player
 	{
 		this.processHorizontalMovement(input, solver)
 		this.processVerticalMovement(input, solver)
+		
+		while (this.position.y > canvasHeight)
+			this.position.y -= canvasHeight
 	}
 	
 	
@@ -42,37 +46,56 @@ class Player
 		if (this.speedGravity.magn() > 20)
 			this.speedGravity = this.speedGravity.norm().scale(20)
 		
+		if (this.speedGravity.y >= 0)
+			this.jumping = false
+		
 		if (this.jumping && !input.up)
 		{
 			this.speedGravity.y *= 0.25
 			this.jumping = false
 		}
 		
-		let raycastDownwards = solver.raycast(this.position.add(new Vec2(0, -1)), new Vec2(0, 1), this.radius)
-		//if (raycastDownwards != null)
-		//	console.log(raycastDownwards.point.sub(this.position).magn().toFixed(8))
+		let raycastYOffset = 1
+		let raycastDownwards = solver.raycast(this.position.add(new Vec2(0, -raycastYOffset)), new Vec2(0, 1), this.radius)
+		let isInside = solver.isInside(this.position, this.radius)
 		
-		let solverResult = solver.solveCircle(this.position, this.speedGravity, this.radius)
-		this.position = solverResult.position
+		let groundDistance = (raycastDownwards == null ? Infinity : raycastDownwards.point.sub(this.position).magn() - raycastYOffset)
+		let groundNormal = (raycastDownwards == null ? new Vec2(0, -1) : raycastDownwards.normal)
+		let groundIsSteep = (groundNormal.dot(new Vec2(0, -1)) < 0.3)
 		
-		if (solverResult.collided)
+		this.onGroundLast = this.onGround
+		this.onGround = (this.speedGravity.y >= 0 && groundDistance < 2)
+		
+		if (this.speedGravity.y >= 0 && !isInside && this.onGroundLast && groundDistance < 20 && raycastDownwards != null)
 		{
-			if (input.up && this.speedGravity.y >= 0)
+			this.position = raycastDownwards.point
+			this.onGround = true
+		}
+		
+		if (this.speedGravity.y < 0 || groundIsSteep)
+		{
+			let solverResult = solver.solveCircle(this.position, this.speedGravity, this.radius)
+			let movedDistance = solverResult.position.sub(this.position).magn()
+			
+			this.position = solverResult.position
+			
+			if (this.speedGravity.y < 0)
+				this.speedGravity = this.speedGravity.norm().scale(Math.min(this.speedGravity.magn(), movedDistance * 2))
+		}
+		else if (this.speedGravity.y > 0)
+		{
+			this.position = this.position.add(this.speedGravity.norm().scale(Math.min(this.speedGravity.magn(), groundDistance)))
+		}
+		
+		if (this.onGround && !groundIsSteep)
+		{
+			this.speedGravity = new Vec2(0, 0)
+			
+			if (input.up)
 			{
 				this.speedGravity = new Vec2(0, -12)
 				this.jumping = true
 			}
-			else
-			{
-				this.speedGravity = new Vec2(0, 0)
-				this.jumping = false
-			}
 		}
-		
-		if (this.speedGravity.y >= 0)
-			this.jumping = false
-		
-		while (this.position.y > canvasHeight)
-			this.position.y -= canvasHeight
 	}
 }
